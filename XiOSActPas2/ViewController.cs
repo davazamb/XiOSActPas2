777f471b04801ce6ee1a27f2be561d4ce363bc5e
David Zambrano <davazamb10@outlook.com>
2016-11-09 21:47:42 -0400

    Leccion 1
    Concluido Proyecto diseño de iOS activos pasivos

--- XiOSActPas2/ViewController.cs
+++ XiOSActPas2/ViewController.cs
@@ -15,6 +15,35 @@ namespace XiOSActPas2
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+			double Caja, Banco, CuentasporCobrar, PagoaCredito, PagoaProveedores, Renta, Capital;
+			btnCalcular.TouchUpInside += delegate {
+				try
+				{
+					Caja = double.Parse(txtCaja.Text);
+					Banco = double.Parse(txtBanco.Text);
+					CuentasporCobrar = double.Parse(txtCuentasporCobrar.Text);
+					PagoaCredito = double.Parse(txtPagoaCredito.Text);
+					PagoaProveedores = double.Parse(txtPagoaProveedores.Text);
+					Renta = double.Parse(txtRenta.Text);
+					Capital = (Caja + Banco + CuentasporCobrar) - (PagoaCredito + PagoaProveedores + Renta);
+					txtCapitalContable.Text = Capital.ToString();
+				}
+				catch (Exception ex)
+				{
+					MessageBox("Error", (ex.Message));
+				}
+			};
+		}
+
+		private void MessageBox(string Title, string message)
+		{
+			using (UIAlertView Alerta = new UIAlertView()) 
+			{
+				Alerta.Title = Title;
+				Alerta.Message = message;
+				Alerta.AddButton("Aceptar");
+				Alerta.Show();
+			}
 		}
 
 		public override void DidReceiveMemoryWarning()
